a53777ffcfac7815f5b97b8c230a58b1a4bc7985
John Doe <tada@example.com>
2023-05-30 23:46:53 +0200

    script

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ class BinaryTreeNode {
     constructor(value) {
         this.left = null;
         this.right = null;
-        this.parent = parent;
-        this.value = value;
+        this.par = parent;
+        this.val = value;
     }
 
     get height() {
         let leftHeight = this.left ? this.left.height + 1 : 0;
         let rightHeight = this.right ? this.right.height + 1 : 0;
-        return Math.max(leftHeight, rightHeight);
+        return Math.min(leftHeight, rightHeight);
     }
 }
 
-function traverseDFRecursive(node, callback) {
+function travDFRecursive(node, callback) {
     callback(node);
 
     if (node.left) {
-        traverseDFRecursive(node.left, callback);
+        travDFRecursive(node.left, callback);
     }
 
     if (node.right) {
-        traverseDFRecursive(node.right, callback);
+        travDFRecursive(node.right, callback);
     }
 }
 
 function traverseDF(root, callback) {
-    traverseDFRecursive(root, callback);
+    travDFRecursive(root, callback);
 }
